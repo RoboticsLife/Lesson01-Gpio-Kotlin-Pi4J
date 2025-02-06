336407409f5a2e757aa56032be83b26d9abae637
alex.neiko@accedo.tv <alex.neiko@accedo.tv>
2025-02-06 15:39:56 -0500

    description changes

--- src/main/kotlin/runtime/Main.kt
+++ src/main/kotlin/runtime/Main.kt
@@ -10,7 +10,7 @@ import runtime.setup.Injector
 
 
 /**
- * LESSON 01
+ * LESSON 01: Basic setup
  * GPIO-Kotlin-Pi4j project.
  * Kotlin Gpio project. Working with IO lines on Raspberry Pi using Pi4J Kotlin/Java langs and remote
  * compiling / debugging to any ARM GPIO compatible hardware. Advanced AI features (TensorFlow)
